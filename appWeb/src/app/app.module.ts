9c07605fa913025cc3d452ac1bef38e6c1c5817e
pepemartinez <josedejesus.martinez@gmail.com>
2019-04-21 14:26:28 +0200

    add material dependences

--- appWeb/src/app/app.module.ts
+++ appWeb/src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+
+
+import { MatCardModule } from '@angular/material/card';
+import { MatMenuModule } from '@angular/material/menu';
 import { AppComponent } from './app.component';
+import { NavigatorComponent} from './navigator/navigator.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, 
+    NavigatorComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatCardModule,
+    MatMenuModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
