e3329a87c8a7dc937f438b2c554f9e69f5cc0b78
pepemartinez <josedejesus.martinez@gmail.com>
2019-04-21 20:06:09 +0200

    boostrap installed

--- appWeb/src/app/app.module.ts
+++ appWeb/src/app/app.module.ts
@@ -2,21 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { 
-  MatToolbarModule 
+  MatToolbarModule, MatCardModule 
 } from '@angular/material';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './header/header.component';
+import { CoverComponent } from './cover/cover.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    CoverComponent
   ],
   imports: [
     BrowserModule,
     MatToolbarModule,
+    MatCardModule,
     BrowserAnimationsModule
   ],
   providers: [],
