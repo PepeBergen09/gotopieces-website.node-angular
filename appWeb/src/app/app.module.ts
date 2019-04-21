7540d2b121f8d0bef246718ff724edcb2afeb920
pepemartinez <josedejesus.martinez@gmail.com>
2019-04-21 15:35:21 +0200

    header section created

--- appWeb/src/app/app.module.ts
+++ appWeb/src/app/app.module.ts
@@ -1,23 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { 
+  MatToolbarModule 
+} from '@angular/material';
 
-
-import { MatCardModule } from '@angular/material/card';
-import { MatMenuModule } from '@angular/material/menu';
 import { AppComponent } from './app.component';
-import { NavigatorComponent} from './navigator/navigator.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent, 
-    NavigatorComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    MatCardModule,
-    MatMenuModule,
+    MatToolbarModule,
     BrowserAnimationsModule
   ],
   providers: [],
