5a977d3af4bd6cc7e4681c1682890ab0108507f9
pepemartinez <josedejesus.martinez@gmail.com>
2019-07-15 18:52:57 +0200

    links in sosial media

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,6 @@ import {
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HeaderComponent } from './header/header.component';
 import { CoverComponent } from './cover/cover.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { CoverSliderComponent } from './cover-slider/cover-slider.component';
@@ -18,11 +17,8 @@ import { MainFeaturesComponent } from './main/main-features/main-features.compon
 import { MainInfoComponent } from './main/main-info/main-info.component';
 import { MainMoreFeaturesComponent } from './main/main-more-features/main-more-features.component';
 import { MainFeatures2Component } from './main/main-features2/main-features2.component';
-import { AboutusComponent } from './aboutus/aboutus.component';
-import { TeamComponent } from './team/team.component';
 import { ContactComponent } from './contact/contact.component';
 import { TestimonialComponent } from './testimonial/testimonial.component';
-import { SubscriptionComponent } from './subscription/subscription.component';
 import { MainFeatures1Component } from './main/main-features1/main-features1.component';
 import { MainFeatures3Component } from './main/main-features3/main-features3.component';
 import { MainTeamComponent } from './main/main-team/main-team.component';
@@ -54,7 +50,6 @@ const appRoutes: Routes = [
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
     CoverComponent,
     NavbarComponent,
     CoverSliderComponent,
@@ -64,11 +59,8 @@ const appRoutes: Routes = [
     MainInfoComponent,
     MainMoreFeaturesComponent,
     MainFeatures2Component,
-    AboutusComponent,
-    TeamComponent,
     ContactComponent,
     TestimonialComponent,
-    SubscriptionComponent,
     MainFeatures1Component,
     MainFeatures3Component,
     MainTeamComponent,
