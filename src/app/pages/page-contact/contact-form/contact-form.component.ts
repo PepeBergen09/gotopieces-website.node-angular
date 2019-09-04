baac4ffa606a51cb3574f65adee98a8a1c9c930f
pepemartinez <josedejesus.martinez@gmail.com>
2019-09-04 19:51:26 +0200

    team photos fixed

--- src/app/pages/page-contact/contact-form/contact-form.component.ts
+++ src/app/pages/page-contact/contact-form/contact-form.component.ts
@@ -6,6 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact-form.component.css']
 })
 export class ContactFormComponent implements OnInit {
+  name: string;
+  email: string;
+  subject: string;
+  message: string;
 
   constructor() { }
 
