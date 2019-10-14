f8cdfdf720989bbd52a8ae05be201aa937176c25
pepeBergen09 <josedejesus.martinez@gmail.com>
2019-10-14 21:08:48 +0200

    Content title  changed

--- src/app/pages/page-news/news-list/news-list.component.ts
+++ src/app/pages/page-news/news-list/news-list.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-news-list',
-  templateUrl: './news-list.component.html'
+  templateUrl: './news-list.component.html',
+  styleUrls: ['./news-list.component.css']
 })
 export class NewsListComponent implements OnInit {
 
